217fefcfb4a0e6521b9eb38984ab14990e7339a4
ADSF <aaron881015@hotmail.com.tw>
2022-01-02 23:52:40 +0800

    Initial commit

---
+++ app/src/main/java/com/example/zaber/MainActivity.java
@@ -0,0 +1,38 @@
+package com.example.zaber;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.util.Log;
+import android.view.Menu;
+
+public class MainActivity extends AppCompatActivity {
+
+    /**
+     * Duration of wait
+     **/
+    private final int SPLASH_DISPLAY_LENGTH = 1000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        /* New Handler to start the Menu-Activity
+         * and close this Splash-Screen after some seconds.*/
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+
+                // Create Connection with DB
+
+                /* Create an Intent that will start the Menu-Activity. */
+                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, SPLASH_DISPLAY_LENGTH);
+    }
+}
