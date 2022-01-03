f24d029ef57865e7cdf5eb164a76caa4e98dc2ca
ADSF <aaron881015@hotmail.com.tw>
2022-01-03 23:00:46 +0800

    [Feature] Login Page View Finish

--- app/src/main/java/com/example/zaber/LoginActivity.java
+++ app/src/main/java/com/example/zaber/LoginActivity.java
@@ -3,12 +3,28 @@ package com.example.zaber;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
+
+import com.google.android.material.textfield.TextInputEditText;
 
 public class LoginActivity extends AppCompatActivity {
+
+    TextView loginTab, registerTab;
+    TextInputEditText email, password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        // Get View Connection
+        loginTab = findViewById(R.id.loginTab);
+        registerTab = findViewById(R.id.registerTab);
+        email = findViewById(R.id.login_mail);
+        password = findViewById(R.id.login_password);
+
+        // Validation
+
+        // Redirection customer or store
     }
 }
